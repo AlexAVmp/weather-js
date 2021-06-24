43533099ee7772389fa14c640fe8273135c9adb2
AlexAVmp <al.averyn.new@gmail.com>
2021-06-24 19:24:52 +0300

    feat: project has been added

---
+++ main.js
@@ -0,0 +1,22 @@
+let inputValue = document.querySelector(".inputValue");
+let button = document.querySelector(".button");
+let cityName = document.querySelector(".name");
+let description = document.querySelector(".desc");
+let temp = document.querySelector(".temp");
+
+button.addEventListener('click', function() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=3707ac5d9ac9e0fa74834751c282d165')
+    .then(response => response.json())
+    .then(data => {
+    let tempValue = data['main']['temp'];
+    let nameValue = data['name'];
+    let descValue = data['weather'][0]['description'];
+
+    cityName.innerHTML = nameValue;
+    description.innerHTML = "Desc - "+descValue;
+    temp.innerHTML = "Temp - "+ (tempValue - 273.15).toFixed(1);
+    input.value ="";
+})
+
+.catch(err => alert("Wrong"));
+})
